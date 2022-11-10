eafc4989c4acb3616304a42067769de6d7129590
Saqib Ahmad <you@example.com>
2022-11-10 21:48:02 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { response } = require('express');
 require('dotenv').config();
 
 const app = express();
@@ -14,6 +14,9 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.csyc5ob.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 async function run() {
     try {
         const serviceCollection = client.db('davidsBurger').collection('services');
@@ -49,10 +52,38 @@ async function run() {
             res.send(result);
         })
 
-        // Getting service wise review
-        app.get('/reviews', async (req, res) => {
-            let query = {};
+        // JWT Token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        })
 
+        // JWT Verifier
+        function verifyJwt(req, res, next) {
+            const authHeader = req.headers.authorization;
+            // console.log(authHeader);
+            if (!authHeader) {
+                return res.status(402).send({ message: 'unauthorized access' });
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' });
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        // Getting service wise review
+        app.get('/reviews', verifyJwt, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+
+            let query = {};
             if (req.query?.email) {
                 query = {
                     email: req.query?.email
@@ -80,7 +111,6 @@ async function run() {
         app.patch('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const status = req.body.newReviewText;
-            console.log(status);
             const query = { _id: ObjectId(id) };
             const updatedDoc = {
                 $set: {
