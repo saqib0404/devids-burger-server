dbb087826e5b40801ffaaaf16c9997197904bd0b
Saqib Ahmad <you@example.com>
2022-11-10 08:34:09 +0600

    mongodb database added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,19 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.csyc5ob.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// function run() {
+//     try {
+
+//     }
+//     finally { }
+// }
+// run().catch(e => console.log(e))
+
 
 app.get('/', (req, res) => {
     res.send("Burger Server running");
